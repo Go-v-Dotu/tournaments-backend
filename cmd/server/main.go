e2139a8fefa035b228903e9e1c4faeb018543894
grinnds <barishevad370@gmail.com>
2024-06-28 22:14:06 +0300

    Add env config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"os"
-
+	"strconv"
 	"tournaments_backend/internal/application"
 	"tournaments_backend/internal/infrastructure/api"
 	"tournaments_backend/internal/infrastructure/events_handler"
@@ -12,32 +13,65 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+func initMongoConfig() (*mongodb.Config, error) {
+	ip := os.Getenv("MONGO_IP")
+	if ip == "" {
+		return nil, errors.New("mongo ip is empty")
+	}
+	port, err := strconv.Atoi(os.Getenv("MONGO_PORT"))
+	if err != nil {
+		return nil, err
+	}
+	user := os.Getenv("MONGO_USER")
+	if user == "" {
+		return nil, errors.New("mongo user is empty")
+	}
+	password := os.Getenv("MONGO_PASSWORD")
+	if password == "" {
+		return nil, errors.New("mongo password is empty")
+	}
+	authSource := os.Getenv("MONGO_AUTH_SOURCE")
+	if authSource == "" {
+		return nil, errors.New("mongo auth source is empty")
+	}
+
+	return &mongodb.Config{
+		IP:         ip,
+		Port:       port,
+		User:       user,
+		Password:   password,
+		AuthSource: authSource,
+	}, nil
+}
+
 func main() {
 	ctx := context.TODO()
 
 	logger := logrus.New()
-	rabbitAddr := "amqp://user:password@127.0.0.1:5672/"
+	rabbitAddr := os.Getenv("RABBIT_ADDR")
+	if rabbitAddr == "" {
+		logger.Fatal("Error during initialization: RABBIT_ADDR env not set")
+	}
+
+	mongoConfig, err := initMongoConfig()
+	if err != nil {
+		logger.Fatalf("Error during initialization: %v", err)
+	}
 
 	app, err := application.NewApp(
 		ctx,
-		mongodb.Config{
-			IP:         "127.0.0.1",
-			Port:       27017,
-			User:       "admin",
-			Password:   "admin",
-			AuthSource: "admin",
-		},
+		*mongoConfig,
 		rabbitAddr,
 		logger,
 	)
 	if err != nil {
-		logger.Fatal("Error during initialization", err)
+		logger.Fatalf("Error during initialization: %v", err)
 		os.Exit(1)
 	}
 
 	eventsHandler, err := events_handler.NewEventsHandler(app, rabbitAddr, logger)
 	if err != nil {
-		logger.Fatal("Error during initialization", err)
+		logger.Fatalf("Error during initialization: %v", err)
 		return
 	}
 
